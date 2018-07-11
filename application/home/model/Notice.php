cc27a1d2f171521dba64150abb6d82230b96df06
Administrator <18096528468@163.com>
2018-07-11 17:46:20 +0800

    随手拍

--- application/home/model/Notice.php
+++ application/home/model/Notice.php
@@ -14,7 +14,7 @@ use think\Model;
 class Notice extends Model {
     //首页获取已推送的数据
     public function get_list($where,$length=0){
-        $list = $this->where($where)->order('create_time','desc')->limit($length,10)->select();
+        $list = $this->where($where)->order('create_time','desc')->limit($length,12)->select();
         foreach($list as $value){
             $value['create_time'] = date('Y-m-d',$value['create_time']);
             $value['front_cover'] = Picture::where('id',$value['front_cover'])->value('path');
